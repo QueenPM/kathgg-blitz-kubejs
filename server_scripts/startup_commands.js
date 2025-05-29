77dd838f63ab1af9848642ba0046bc17a6799410
QueenPM <danimark644@gmail.com>
2025-05-29 04:54:16 +0100

    feat: add playersSleepingPercentage gamerule to startup commands

--- server_scripts/startup_commands.js
+++ server_scripts/startup_commands.js
@@ -1,6 +1,7 @@
 const STARTUP_CMDS = [
   "hiddennames blocksHideName true",
-  "gamerule disableElytraMovementCheck true"
+  "gamerule disableElytraMovementCheck true",
+  "gamerule playersSleepingPercentage 50"
 ]
 
 
