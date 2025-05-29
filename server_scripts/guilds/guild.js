fa7967798489e7d364b857ba7dab3a496a595b7d
QueenPM <danimark644@gmail.com>
2025-05-29 17:59:53 +0100

    fix: fixed writing and reading issues with guild data

--- server_scripts/guilds/guild.js
+++ server_scripts/guilds/guild.js
@@ -3,14 +3,21 @@ let GUILDS = null
 const GUILDS_PATH = "kubejs/data/guilds.json";
 
 function loadGuilds() {
-  GUILDS = JsonIO.read(GUILDS_PATH);
-  if (!GUILDS) {
+  console.log("Loading Guilds")
+  GUILDS = {};
+  let guildsData = JsonIO.read(GUILDS_PATH);
+  if (!guildsData) {
     JsonIO.write(GUILDS_PATH, "{}");
   }
-  GUILDS = JsonIO.read(GUILDS_PATH);
+  guildsData = JsonIO.read(GUILDS_PATH);
+  for(const g of guildsData.values()){
+    GUILDS[g.id] = g
+  }
+  console.log(GUILDS)
 }
 
 function saveGuilds() {
+  console.log("Saving Guilds")
   if (!GUILDS) loadGuilds();
   syncGuilds();
   JsonIO.write(GUILDS_PATH, GUILDS);
@@ -31,15 +38,18 @@ function syncGuilds() {
   }
 
   for(const guildIdToClear of toClear){
+    console.log("DELETING")
     delete GUILDS[guildIdToClear]
   }
 
   // Go through all guilds and compare it to the one in memory
   for(const guild of guilds){
+    console.log(GUILDS)
     let guildExists = GUILDS[guild.id];
 
     // If the guild does not exist, add it to memory
     if(!guildExists){
+      console.log("MAKING NEW")
       GUILDS[guild.id] = guild
       continue;
     }
@@ -48,6 +58,8 @@ function syncGuilds() {
     guildExists.settings = guild.settings;
     guildExists.owner = guild.owner;
     guildExists.members = guild.members;
+
+    console.log("updating :)")
   }
 }
 
@@ -56,6 +68,8 @@ function syncGuilds() {
  * @param {GuildInformation} guild 
  */
 function saveGuildData(guild){
+  if(!GUILDS) loadGuilds();
   // Race conditions but w/e really.
   GUILDS[guild.id] = guild;
+  console.log(GUILDS)
 }
