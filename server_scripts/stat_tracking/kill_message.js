65f73ce0889b4adc27fa678d647083c035f4ca36
QueenPM <danimark644@gmail.com>
2025-06-08 13:05:14 +0100

    refactor: updated kill_message

--- server_scripts/stat_tracking/kill_message.js
+++ server_scripts/stat_tracking/kill_message.js
@@ -1,6 +1,6 @@
 // priority: 0
 
-const MIN_LENGTH = 0;
+const BROADCAST_DISTANCE_MIN_LENGTH = 5;
 
 const ONLY_PLAYER_KILLS = false;
 
@@ -18,33 +18,20 @@ EntityEvents.death((event)=>{
   let deadPlayer =  event.server.getPlayer(event.entity.username);
   // If the entity is not a player, return
   if(!deadPlayer && ONLY_PLAYER_KILLS) return;
-  if(deadPlayer){
-    addDeath(deadPlayer, event.source);
-  }
-  // Get the source, aka the killer
+
+  // Get the Kill Distance
   
   let deadPlayerPos = event.entity.position();
   let killerPlayerPos = killerPlayer.position();
   let distance = deadPlayerPos.distanceTo(killerPlayerPos);
-  
-  let uuid = "";
-  let name = "";
-  if(deadPlayer){
-    uuid = deadPlayer.getStringUuid();
-    name = deadPlayer.username;
-  }else{
-    uuid = event.entity.getStringUuid();
-    name = event.entity.getEntityType().toString();
-  }
-  
-  if(!uuid || !name) return;
 
-  /**@type {$ItemStack_} */
+  /**@type {$ItemStackKJS_} */
   let weaponUsed = killerPlayer.handSlots[0];
 
+  // Add Stats for Killer and Killed
   addKill(killerPlayer, {
-    uuid:uuid,
-    name:name,
+    uuid: event.entity.getStringUuid(),
+    name: deadPlayer ? deadPlayer.username : event.entity.getEntityType().toString(),
     weapon: weaponUsed ? {
       id: weaponUsed.id,
       count: weaponUsed.count,
@@ -55,45 +42,88 @@ EntityEvents.death((event)=>{
     timestamp: Date.now()
   });
 
-  if(distance < MIN_LENGTH) return;
-
-  let itemComponent = itemToChatComponent(killerPlayer.handSlots[0]);
-
-  let glyphs = "";
-  let spellUsed = getSelectedSpell(weaponUsed);
+  if(deadPlayer){
+    addDeath(deadPlayer, event.source);
+  }
+  
+  // Create Tellraw Command
+  
+  /**
+   * @type {TextComponent[]}
+  */
+ let tellRawComponents = [
+  // { text: "☠ 🏹 ", color: "dark_red"},
+  
+   getPlayerChatName(killerPlayer), // Player Name Plate
+   { text: " has slain ", color: "gray" },
+   getPlayerChatName(deadPlayer || event.entity) // Killed Player/Entity Name Plate
+  ];
+  
+  tellRawComponents.push(
+    { text: ` using `, color: "gray" }
+  )
+  
+  // Ars Nouveau
+  const spellUsed = getSelectedSpell(weaponUsed);
   if(spellUsed){
-    let displayComponent = `lore=['["", {"text":"${spellUsed.glyphs}"}]']`
-    let item = Item.of(`${killerPlayer.handSlots[0].id}[${displayComponent}]`);
-    glyphs = `, {"text": " §8(Spell§8)", "hoverEvent": { "action": "show_item", "contents":${itemToChatComponent(item)}}}`
-  }
-  let tellraw = `tellraw @a {"text": "", "extra": [${JSON.stringify(getPlayerChatName(killerPlayer))},{"text":" has slain ", "color":"dark_red"}, ${JSON.stringify(getPlayerChatName(deadPlayer || event.entity))}, {"text": " §8(§e${distance.toFixed(2)} blocks§8)"`
-
-  if(itemComponent){
-    tellraw+= `, "hoverEvent": { "action": "show_item", "contents":${itemComponent}}}${glyphs}]}`
+    let glyphs = Item.of(`${spellUsed.recipe[0].item.id}[hide_additional_tooltip={},custom_name='${JSON.stringify({text:spellUsed.name || "Spell", italic: false})}',lore=['["", ${JSON.stringify(colorSpellGlyphs(spellUsed.recipe))}]']]`)
+    tellRawComponents.push(
+      { text: spellUsed.name || spellUsed.recipe[0].name, color: "dark_purple", hoverEvent: { action: "show_item", contents: glyphs.toJson()}}
+    )
   }else{
-    tellraw += `}]}`;
+    // Get the Mainhand Weapon
+    let weaponName = weaponUsed.getDisplayName().getString()
+    weaponName = weaponName.substring(1, weaponName.length() - 1);
+    
+    tellRawComponents.push(
+      { text: weaponName, hoverEvent: { action: "show_item", contents: weaponUsed.toJson() }}
+    )
   }
-  event.server.runCommandSilent(tellraw)
+  
+  // Get the distance for the kill and add it if its beyond BROADCAST_DISTANCE_MIN_LENGTH
+  if(distance >= BROADCAST_DISTANCE_MIN_LENGTH){
+    tellRawComponents.push(
+      { text: ` §8(§e${distance.toFixed(2)} blocks§8)` }
+    )
+  }
+
+  // Killstreak
+  let playerData = getPlayerData(killerPlayer.uuid)
+  if(playerData && playerData.killstreak){
+    tellRawComponents.push(
+      { text: " ★ x", color: "yellow"},
+      { text: `${playerData.killstreak}`, color: "yellow"},
+    )
+  }
+
+  const tellrawcmd = `tellraw @a {"text": "", "extra": ${JSON.stringify(tellRawComponents)}}`
+
+  event.server.runCommandSilent(tellrawcmd)
+
+  // In case Keep Inventory is ON! Drop All (Canceling event)
   if(deadPlayer && !event.server.getGameRules().get("keepInventory")) deadPlayer.inventory.dropAll();
 
-  if (!FEATURE_CREDITS) return;
-  
-  if (arePlayersAllies(deadPlayer || event.entity, killerPlayer)) return;
-  
-  let creditsToGive;
-  if(distance < MIN_DISTANCE_CREDITS_FOR_KILLS) {
-    creditsToGive = CREDITS_FOR_KILLS
+  // Give Credits
+  if (FEATURE_CREDITS) {
+    // If players are NOT allies
+    if (!arePlayersAllies(deadPlayer || event.entity, killerPlayer)){
+      let creditsToGive;
+      if(distance < MIN_DISTANCE_CREDITS_FOR_KILLS) {
+        creditsToGive = CREDITS_FOR_KILLS
+      }
+      else if (distance >= MAX_DISTANCE_CREDITS_FOR_KILLS) {
+        creditsToGive = CREDITS_FOR_KILLS_MAX;
+      } else {
+        creditsToGive =
+        CREDITS_FOR_KILLS +
+        (CREDITS_FOR_KILLS_MAX - CREDITS_FOR_KILLS) *
+        (distance / MAX_DISTANCE_CREDITS_FOR_KILLS);
+      }
+      creditsToGive = Math.round(creditsToGive);
+      
+      giveCredits(killerPlayer, creditsToGive);
+    }
   }
-  else if (distance >= MAX_DISTANCE_CREDITS_FOR_KILLS) {
-    creditsToGive = CREDITS_FOR_KILLS_MAX;
-  } else {
-    creditsToGive =
-    CREDITS_FOR_KILLS +
-    (CREDITS_FOR_KILLS_MAX - CREDITS_FOR_KILLS) *
-    (distance / MAX_DISTANCE_CREDITS_FOR_KILLS);
-  }
-  creditsToGive = Math.round(creditsToGive);
   
-  giveCredits(killerPlayer, creditsToGive);
   event.cancel();
 })
