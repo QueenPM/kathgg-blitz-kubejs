7a655310f3cda6f1d115cc739f63d0c3c55090ab
QueenPM <danimark644@gmail.com>
2025-06-15 16:14:15 +0100

    refactor(wip): move scoreboard from credits into its own folder.

--- server_scripts/credits/credits.js
+++ server_scripts/credits/credits.js
@@ -91,51 +91,6 @@ function setCredits(player, credits) {
   }
 }
 
-ServerEvents.tick((e) => {
-  if (!FEATURE_CREDITS) return;
-  if (e.server.tickCount % 80 != 0) return;
-
-  let leaderboardData = getLeaderboard().sort((a, b) => a.credits < b.credits);
-
-  const objectiveName = "bb_leaderboard";
-  const objectiveDisplayName = `{"text":" Credits ", "color":"yellow"}`;
-
-  const setupCommands = [
-    `scoreboard objectives remove ${objectiveName}`,
-    `scoreboard objectives add ${objectiveName} dummy ${objectiveDisplayName}`,
-    `scoreboard objectives setdisplay sidebar ${objectiveName}`,
-  ];
-
-  for (const cmd of setupCommands) {
-    e.server.runCommandSilent(cmd);
-  }
-
-  if (leaderboardData && leaderboardData.length > 0) {
-    for (let i = 0; i < leaderboardData.length; i++) {
-      let entryData = leaderboardData[i];
-      let actualPlayerName = entryData.name;
-      let credits = entryData.credits;
-
-      /** @type {TextComponent[]} */
-      let displayNameJsonComponent = [
-        { text: `${actualPlayerName} ` },
-        { text: `◎${numbersWithCommas(credits)}`, color: "yellow" },
-      ];
-
-      let displayNameJsonString = JSON.stringify(displayNameJsonComponent);
-
-      let score = i;
-      e.server.runCommandSilent(
-        `scoreboard players set ${actualPlayerName} ${objectiveName} ${score}`
-      );
-      e.server.runCommandSilent(
-        `scoreboard players display name ${actualPlayerName} ${objectiveName} ${displayNameJsonString}`
-      );
-    }
-  }
-});
-
-
 ServerEvents.commandRegistry((event) => {
   const { commands: Commands, arguments: Arguments } = event;
 
